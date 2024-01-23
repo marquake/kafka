0872e0d373e32e9d4acc7b76f0a0a785a49c5480
mms <marcosms@gmail.com>
2024-01-23 21:42:06 +0100

    Se ha Creado un Consumidor/Productor de Kafka. Esto genera un UUID por segundo que se va consumiendo de la cola. En el docker-compose.yaml se ha creado la cola con Kafdrop.

---
+++ kafka-consumer-intellJ-Thread/src/main/java/com/mms/kakfa_consumer/KafkaConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.mms.kakfa_consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	@Autowired
+	static KafkaMessageConsumer kafkaMessageConsumer;
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+
+	}
+
+}
