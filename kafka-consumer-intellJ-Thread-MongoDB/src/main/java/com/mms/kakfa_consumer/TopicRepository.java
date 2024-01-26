4b416cefd519860ee8d7932b4dbbaf8f3d4bfde3
mms <marcosms@gmail.com>
2024-01-26 08:20:37 +0100

    Modificación: Este objeto se deja como estaba originalmente. Es un objeto topic que se guarda en la tabla de Topics. tiene dos datos de usuario pero se utiliza para ver que podemos dar de alta un objeto que viene de una cola en base de datos.

--- kafka-consumer-intellJ-Thread-MongoDB/src/main/java/com/mms/kakfa_consumer/TopicRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mms.kakfa_consumer;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TopicRepository extends MongoRepository<Topic, String> {
-}
